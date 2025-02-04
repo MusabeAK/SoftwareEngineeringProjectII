997037fb6fb189d4902518ce5e685f6a8f00d0fc
katocollins <ckato131@gmail.com>
2025-02-04 22:17:56 +0300

    feat: Reintialized the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,7 @@
-import React, { useState } from "react";
-import "./App.css"; // Custom CSS
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./components/Home";
-import Tasks from "./components/tasks/Tasks";
-import Contacts from "./components/Contact";
-import Document from "./components/documents/Documents";
-
 function App() {
-  // State to hold the current content type
-  const [content, setContent] = useState("home");
-
-  // Function to update content based on the clicked link
-  const handleNavigation = (view) => {
-    setContent(view); // Update the state with the view
-  };
   return (
     <>
-      <Header handleNavigation={handleNavigation} />
-      <div className="App">
-        <main>
-          <article>
-            {content === "home" && <Home />}
-            {content === "tasks" && <Tasks />}
-            {content === "documents" && <Document />}
-            {content === "contact" && <Contacts />}
-          </article>
-        </main>
-
-        <Footer />
-      </div>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
